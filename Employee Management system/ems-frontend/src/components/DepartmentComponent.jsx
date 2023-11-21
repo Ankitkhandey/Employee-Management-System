ee3bbab935f041b5f222102c4826cf3ae40b76f2
Ankit Khandey <ankitkhandey123@gmail.com>
2023-11-21 21:31:15 +0530

    basic form for add department

--- Employee Management system/ems-frontend/src/components/DepartmentComponent.jsx
+++ Employee Management system/ems-frontend/src/components/DepartmentComponent.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DepartmentComponent = () => {
-  return (
-    <div>
-      DepartmentComponent
+
+    const [departmentName, setDepartmentName]=useState('');
+    const [departmentDescription, setDepartmentDescription]= useState('');
+    return (
+    <div className='container'>
+    <div className='row'>
+        <div className='card col-md-6 offset-md-3 offset-md-3'>
+            <h2 className='text-center'>Add Department</h2>
+
+            <div className='card-body'>
+                <form>
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>Department Name:</label>
+                        <input 
+                        type='text'
+                        name='departmentName'
+                        placeholder='Enter department'
+                        value={departmentName}
+                        onChange={()=> setDepartmentName(e.target.value)}
+                        className='form-control'
+                        >
+                        </input>
+                    </div>
+
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>Department Description</label>
+                        <input 
+                        type='text'
+                        name='departmentDescription'
+                        placeholder='Department Description'
+                        value={departmentDescription}
+                        onChange={()=> setDepartmentDescription(e.target.value)}
+                        className='form-control'
+                        >
+                        </input>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-  )
+    </div>
+    )
 }
 
 export default DepartmentComponent
